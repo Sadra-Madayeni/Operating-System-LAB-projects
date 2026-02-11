3d9ea0b7190a9fb3c41778e0fc71d283bc392d02
Sadra-Madayeni <sadra.madayeni@gmail.com>
2026-02-11 22:49:09 +0330

    Fixed nested codes folder

--- LAB1/codes/bootmain.c
+++ LAB1/codes/bootmain.c
@@ -3,7 +3,7 @@
 // Part of the boot block, along with bootasm.S, which calls bootmain().
 // bootasm.S has put the processor into protected 32-bit mode.
 // bootmain() loads an ELF kernel image from the disk starting at
-// sector 1 and then jumps to the kernel entry routine.
+
 
 #include "types.h"
 #include "elf.h"
